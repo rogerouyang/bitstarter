ee895b39d7682c6b880f603bc455d71ad7e8547f
Ubuntu <ubuntu@ip-172-31-18-116.us-west-2.compute.internal>
2013-07-21 23:49:09 +0000

    modify index

--- web.js
+++ web.js
@@ -1,13 +1,13 @@
 var express = require('express');
-
+var fs=require('fs');
 var app = express.createServer(express.logger());
-var fs = require('fs');
-var inhtml=new Buffer(fs.readFileSync('./index.html'));
+var htmlfile="index.html";
 app.get('/', function(request, response) {
-  response.send(inhtml.toString('utf-8'));
+ var html=fs.readFileSync(htmlfile).toString(); 
+ response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
